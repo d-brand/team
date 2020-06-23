2555d4fd0c54ac10ccc1730a22388143b8adeff4
d-brand <kawasaki@d-brand.jp>
2020-06-23 18:10:44 +0900

    indexの変更

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -33,7 +33,7 @@ class SchedulesController < ApplicationController
     if params[:id]
       schedule = Schedule.find(params[:id])
     else
-      schedule = Schedule.uketsukechu.order(ymd: asc).first
+      schedule = Schedule.uketsukechu.order(ymd: :asc).first
     end
 
     @answers_all = schedule.answer
@@ -44,14 +44,6 @@ class SchedulesController < ApplicationController
 
     @answer = Answer.new
     @answer.schedule_id = params[:id]
-
-    @options = { join: @answers_sanka&.count, maybe: @answers_tabun&.count, pending: @answers_mitei&.count, absence: @answers_kesseki&.count }
-
-    @total = [@answers_sanka&.count, @answers_tabun&.count, @answers_mitei&.count, @answers_kesseki&.count]
-    @join = @total[0]
-    @maybe = @total[1]
-    @pending = @total[2]
-    @absence = @total[3]
 
     @caution = '※チーム毎でサイト管理している為、パスワードを設けておりません。チーム幹事のみ編集してください。'
   end
@@ -113,7 +105,12 @@ class SchedulesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_schedule
-      @schedule = Schedule.find(params[:id])
+      # @schedule = Schedule.find(params[:id])
+      @schedule = if params[:id].blank?
+        Schedule.uketsukechu.first
+      else
+        Schedule.find(params[:id])
+      end
     end
 
     # Only allow a list of trusted parameters through.
