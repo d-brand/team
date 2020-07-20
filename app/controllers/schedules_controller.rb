8a31ccc5106bd8645a90571d27f3d5e03238f9a1
d-brand <kawasaki@d-brand.jp>
2020-07-20 14:46:42 +0900

    no message

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -55,6 +55,7 @@ class SchedulesController < ApplicationController
   # GET /schedules/new
   def new
     @schedules = Schedule.where("schedules.ymd > ?", Time.current.yesterday).reorder(:ymd)
+    @schedules = Schedule.all.order(ymd: "ASC")
     @schedules = Schedule.all
     @schedule = Schedule.new
   end
